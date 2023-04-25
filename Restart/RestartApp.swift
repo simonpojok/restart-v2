edb0a5bcbbbe265d7920538c06215e58559aa50c
simonpojok <simonpojok@gmail.com>
2023-04-25 22:09:45 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Simon Peter Ojok on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
